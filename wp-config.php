91079d5da9b61f8e7feb9eca31c2844e8bee8d4f
arakawa asuka <asuka@localhost.localdomain>
2024-03-26 19:22:34 +0900

    modify wp-config.php add site_home and site_url to env

--- wp-config.php
+++ wp-config.php
@@ -42,6 +42,10 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+/** parameter */
+define( 'WP_HOME', getenv("WP_HOME") );
+define( 'WP_SITEURL', getenv("WP_SITEURL") );
+
 /**#@+
  * Authentication unique keys and salts.
  *
